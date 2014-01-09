165e4337735e94829dfc4d573ef2b30d7748412e
kovalikp <kovalikp@gmail.com>
2014-01-09 22:48:58 +0100

    Updated version to 1.0.0

--- ServiceModel.Composition/src/SharedAssemblyVersionInfo.cs
+++ ServiceModel.Composition/src/SharedAssemblyVersionInfo.cs
@@ -3,5 +3,5 @@
 #if !BUILD_GENERATED_VERSION
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha1")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
 #endif
